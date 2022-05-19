64e3caad8199277c8c099ae05089a9e8900be28b
yusifizcode <5ba0gh2@code.edu.az>
2022-05-19 20:34:14 +0400

    js file-input task

--- JS-FileInput/js/main.js
+++ JS-FileInput/js/main.js
@@ -15,15 +15,22 @@ fileInput.addEventListener('change',function(){
             images.style.width = 200 + 'px';
             images.style.height = 100 + 'px';
             images.style.display = 'inline-block';
+            images.style.marginRight = 10 + 'px';
             let span = document.createElement('span');
             span.innerText = 'X';
             span.style.position = 'absolute';
-            span.style.right = 4 + 'px';
-            span.style.color = 'white';
+            span.style.right = -4 + 'px';
+            span.style.top = -12 + 'px';
+            span.style.color = 'black';
+            span.style.fontWeight = 'bolder';
+            span.style.cursor = 'pointer';
             img.setAttribute('src',reader.result);
             document.body.appendChild(images);
             images.appendChild(img);
             images.appendChild(span);
+            span.addEventListener('click',function(){
+                images.remove();
+            })
         }
 
         reader.readAsDataURL(file);
