ab0052ffaf2f63a70bc4e1fe847eb34bbaaa87ef
yusifizcode <5ba0gh2@code.edu.az>
2022-05-24 19:11:35 +0400

    fiorello site

--- FiorelloSite/js/main.js
+++ FiorelloSite/js/main.js
@@ -1,18 +1,5 @@
 $(function(){
-    // [...$('.nav-item')].forEach(elem=>{
-    //     $(elem).hover(function(){
-    //         // [...$('.nav-dropdown')].forEach(el=>{
-    //         //     $(el).slideToggle();
-    //         //     console.log(el)
-    //         // })
-    //         console.log($(this).attr('data-id'));
-    //         let dataId = $(this).attr('data-id');
-    //         let ul = $('#'+dataId);
-    //         ul.slideToggle();
-    //         ul.hover(function(){
-    //             ul.css({"display":"block"})
-    //             console.log("salamm");
-    //         })
-    //     })
-    // })
+    $('.search-icon').click(function(){
+        $('.search-sec').toggleClass('active-search');
+    })
 })
