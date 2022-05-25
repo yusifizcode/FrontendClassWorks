4332ea67255a9d42bd6e112d81a1441aa61fef01
yusifizcode <5ba0gh2@code.edu.az>
2022-05-25 14:28:08 +0400

    fiorello site

--- FiorelloSite/js/main.js
+++ FiorelloSite/js/main.js
@@ -2,4 +2,16 @@ $(function(){
     $('.search-icon').click(function(){
         $('.search-sec').toggleClass('active-search');
     })
+
+    var $products = $('.products').isotope({
+        // options
+      });
+      // filter items on button click
+      $('.flowers-list').on( 'click', 'li', function() {
+        var filterValue = $(this).attr('data-filter');
+        // console.log($(this)[0]);
+        $('.flower-item').removeClass('active');
+        $(this).addClass('active');
+        $products.isotope({ filter: filterValue });
+      });
 })
